7f0642dba014c33fdab6f9e00e1f31ba41b65adb
devblocks42 <devblocks42@keemail.me>
2025-06-25 17:32:34 +0200

    - Ajout du test unitaire sur l'objet WeatherIcon

--- src/test/java/com/weatherreport/model/WeatherIconTest.java
+++ src/test/java/com/weatherreport/model/WeatherIconTest.java
@@ -2,7 +2,10 @@ package com.weatherreport.model;
 
 import com.weatherreport.DAL.Repository;
 import com.weatherreport.DAL.WeatherIconRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import javafx.application.Platform;
+import javafx.scene.image.Image;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,8 +26,14 @@ public class WeatherIconTest {
         
     }
     
-    @BeforeAll
-    public static void setUpClass() {
+    
+   @BeforeAll
+    public static void setUpClass() throws Exception {
+        CountDownLatch latch = new CountDownLatch(1);
+        Platform.startup(latch::countDown);
+        if (!latch.await(5, TimeUnit.SECONDS)) {
+            throw new RuntimeException("JavaFX platform failed to start.");
+        }
         WeatherIconRepository weatherIconRepo = new WeatherIconRepository(Repository.getInstance());
         instance0 = weatherIconRepo.getWeatherIcon(0);
         instance1 = weatherIconRepo.getWeatherIcon(1);
@@ -70,15 +79,34 @@ public class WeatherIconTest {
     /**
      * Test of getImage method, of class WeatherIcon.
      */
-    /*@Test
+    @Test
     public void testGetImage() {
         System.out.println("getImage");
-        WeatherIcon instance = null;
-        Image expResult = null;
-        Image result = instance.getImage();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }*/
+        //0
+        WeatherIcon instance_0 = instance0;
+        boolean expResult_0 = false;
+        Image result_0 = instance_0.getImage();
+        assertEquals(expResult_0, result_0.isError());
+        //1
+        WeatherIcon instance_1 = instance1;
+        boolean expResult_1 = false;
+        Image result_1 = instance_1.getImage();
+        assertEquals(expResult_1, result_1.isError());
+        //2
+        WeatherIcon instance_2 = instance2;
+        boolean expResult_2 = false;
+        Image result_2 = instance_2.getImage();
+        assertEquals(expResult_2, result_2.isError());
+        //3
+        WeatherIcon instance_3 = instance3;
+        boolean expResult_3 = false;
+        Image result_3 = instance_3.getImage();
+        assertEquals(expResult_3, result_3.isError());
+        //45
+        WeatherIcon instance_45 = instance45;
+        boolean expResult_45 = false;
+        Image result_45 = instance_45.getImage();
+        assertEquals(expResult_45, result_45.isError());
+    }
     
 }
